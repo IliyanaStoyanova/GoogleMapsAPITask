d8db4b38e7f6ea0e403ddb13a01974263ea6b2fe
Iliyana Stoyanova <iliyana.t.stoyanova@gmail.com>
2021-02-07 06:13:21 +0200

    Correction of syntax errors and add comments.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('registerForm');
 
+/*Error function*/
 function invalid(input, message){
     input.className = 'invalid';
 
@@ -8,6 +9,7 @@ function invalid(input, message){
     return false;
 }
 
+/*Success function*/
 function valid(input){
     input.className = 'valid';
 
@@ -16,32 +18,36 @@ function valid(input){
     return true;
 }
 
+/*Check Required fields */
 function isEmpty(input, message) {
     return input.value.trim() === '' ? invalid(input, message) : valid(input);
 }
 
+/*Validation for Special characters*/
 function specialChar(input, message){
     const regExp =  /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?0-9]/;
     return regExp.test(input.value.trim()) ? invalid(input, message) : valid(input);
 }
 
+/*Email validation*/
 function validateEmail(input) {
     const regExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)| + (".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regExp.test(input.value.trim()) ? valid(input) : invalid(input, 'Invalid email format');
 }
 
+/*Phone validation*/
 function validatePhone(input) {
-    const regExp = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\\./0-9]*$/g
-    // const regExp = /[+-0-9]/;
-    // const regExp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    const regExp = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\\./0-9]*$/g;
     return regExp.test(input.value.trim()) ? valid(input) : invalid(input, 'Invalid phone format');
 }
 
+/*Url validation*/
 function validateUrl(input) {
     const regExp =  /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
     return regExp.test(input.value.trim()) ? valid(input) : invalid(input, 'Invalid url format');
 }
 
+/*Submit Form*/
 form.addEventListener('submit', (event) => {
     let flEmpty=flName=flEmail=flPhone=flWebsite=true;
     let valid;
